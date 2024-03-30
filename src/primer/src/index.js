9efed93922f5b43d17f40c461072fcb0809d4919
Felipe Romero <feliperomero3@outlook.com>
2024-03-30 17:43:56 -0600

    Converting and filtering parameters in the index.js file in the 'primer' project.

--- src/primer/src/index.js
+++ src/primer/src/index.js
@@ -5,12 +5,12 @@ console.log(`Boots price: ${bootsPrice}`); // Boots price: 100
 
 function sumPrices(...numbers) {
   return numbers.reduce(function(total, val) {
-    return total + val
+    return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
   }, 0);
 }
 let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 1001000 string
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 200 number
 totalPrice = sumPrices(100, 200, 300);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 600 number
-totalPrice = sumPrices(100, 200);
+totalPrice = sumPrices(100, 200, undefined, false, "hello");
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 300 number
