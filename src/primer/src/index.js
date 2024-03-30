af36d04894d1b136bcd0b8865a34c7f30ac48520
Felipe Romero <feliperomero3@outlook.com>
2024-03-30 17:21:41 -0600

    Defining a function in the index.js file in the 'primer' project.

--- src/primer/src/index.js
+++ src/primer/src/index.js
@@ -2,3 +2,9 @@ let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
+
+function sumPrices(first, second, third) {
+  return first + second + third;
+}
+let totalPrice = sumPrices(hatPrice, bootsPrice);
+console.log(`Total Price: ${totalPrice}`); // Total Price: 100100undefined
