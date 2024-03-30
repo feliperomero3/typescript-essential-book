e3c2cf6d7a85fe08bbaeb7698818ea3ce049a1f3
Felipe Romero <feliperomero3@outlook.com>
2024-03-30 17:34:21 -0600

    Using a default parameter value in the index.js file in the 'primer' project.

--- src/primer/src/index.js
+++ src/primer/src/index.js
@@ -3,13 +3,12 @@ console.log(`Hat price: ${hatPrice}`); // Hat price: 100
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`); // Boots price: 100
 
-function sumPrices(first, second, third) {
+function sumPrices(first, second, third = 0) {
   return first + second + third;
 }
 let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 100100undefined string
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 1001000 string
 totalPrice = sumPrices(100, 200, 300);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 600 number
 totalPrice = sumPrices(100, 200);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: NaN number
-// JavaScript coalesces 'undefined' to the special number value 'NaN'.
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 300 number
