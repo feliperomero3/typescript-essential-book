c0e3278c05b5ff8cf2a2ccb359959b6b68e2d072
Felipe Romero <feliperomero3@outlook.com>
2024-03-30 17:05:15 -0600

    Preventing coercion in the index.js file in the 'primer' project.

--- src/primer/src/index.js
+++ src/primer/src/index.js
@@ -3,12 +3,12 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
   console.log("Prices are the same");
 } else {
   console.log("Prices are different");
 }
-let totalPrice = hatPrice + bootsPrice;
+let totalPrice = hatPrice + Number(bootsPrice);
 console.log(`Total Price: ${totalPrice}`);
 
 let myVariable = "Felipe";
