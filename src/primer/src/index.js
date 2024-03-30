d3f3dd9c539159a62bd36eb72689652a91df0bc4
Felipe Romero <feliperomero3@outlook.com>
2024-03-30 17:32:03 -0600

    Invoking a function in the 'primer' project.

--- src/primer/src/index.js
+++ src/primer/src/index.js
@@ -1,10 +1,15 @@
 let hatPrice = 100;
-console.log(`Hat price: ${hatPrice}`);
+console.log(`Hat price: ${hatPrice}`); // Hat price: 100
 let bootsPrice = "100";
-console.log(`Boots price: ${bootsPrice}`);
+console.log(`Boots price: ${bootsPrice}`); // Boots price: 100
 
 function sumPrices(first, second, third) {
   return first + second + third;
 }
 let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Total Price: ${totalPrice}`); // Total Price: 100100undefined
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 100100undefined string
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: 600 number
+totalPrice = sumPrices(100, 200);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`); // Total: NaN number
+// JavaScript coalesces 'undefined' to the special number value 'NaN'.
